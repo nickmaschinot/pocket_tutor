23aec2e115b7d2bc239599811179d4de7e3cb3d3
Nick M <nicholas.maschinot@gmail.com>
2017-05-28 23:23:15 +0000

    Add full_title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,7 +10,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Pocket Tutor"
+    assert_select "title", "Pocket Tutor"
   end
 
   test "should get help" do
