fb5fa170dc042e2b944b9c3911d8bdc2eefb954b
Nick M <nicholas.maschinot@gmail.com>
2017-06-05 17:10:46 +0000

    implement login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user 
       flash[:success] = "Welcome to Pocket Tutor!"
       redirect_to @user
     else
