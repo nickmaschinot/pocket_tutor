bc79c83c1572537b9164758c1fac3df79cb12769
Nick M <nicholas.maschinot@gmail.com>
2017-06-09 21:06:57 +0000

    finish user model

---
+++ test/integration/users_index_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class UsersIndexTest < ActionDispatch::IntegrationTest
+  def setup 
+    @admin = users(:nick)
+    @non_admin =users(:lili)
+  end 
+  
+  test "index including pagination" do 
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    first_page_of_users = User.paginate(page:1)
+    first_page_of_users.each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
+    end
+    assert_difference 'User.count', -1 do 
+      delete user_path(@non_admin)
+    end 
+  end
+end
