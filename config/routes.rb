6687a9c99960f70355022f435c6e9a0766eaa57f
Nick M <nicholas.maschinot@gmail.com>
2017-07-13 19:59:54 +0000

    cleaning up files

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,26 @@
 Rails.application.routes.draw do
 
+  get 'questions/new'
+
   root   'static_pages#home'
-  get    '/help',     to: 'static_pages#help'
-  get    '/about',    to: 'static_pages#about'
-  get    '/contact',  to: 'static_pages#contact'
-  get    '/signup',   to: 'users#new'
-  get    '/login',    to: 'sessions#new'
-  post   '/login',    to: 'sessions#create'
-  delete '/logout',   to: 'sessions#destroy'
+  get    '/help',          to: 'static_pages#help'
+  get    '/about',         to: 'static_pages#about'
+  get    '/contact',       to: 'static_pages#contact'
+  get    '/signup',        to: 'users#new'
+  get    '/login',         to: 'sessions#new'
+  get    '/tests',         to: 'tests#new'
+  get    '/subjects',      to: 'tests#subjects'
+  get    '/sat',           to: 'test_pages#sat'
+  get    '/act',           to: 'test_pages#act'
+  get    '/psat',          to: 'test_pages#psat'
+  get    '/biology',       to: 'test_pages#act'
+  get    '/us_history',    to: 'test_pages#us_history'
+  get    '/new_questions', to: 'questions#new'
+  post   '/add_question',  to: 'questions#create'
+  post   '/login',         to: 'sessions#create'
+  delete '/logout',        to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :questions,           only: [:new, :destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614174547) do
+ActiveRecord::Schema.define(version: 20170622202637) do
+
+  create_table "questions", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name"
+    t.index ["user_id", "created_at"], name: "index_questions_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.string   "name"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
